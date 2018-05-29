c635ac31b9b2a8bdc54c9c9477e73f48c5e3e94e
twoshoe <twoshoe@gmail.com>
2018-05-29 19:02:43 -0400

    Trimmed

--- 6768.py
+++ 6768.py
@@ -10,14 +10,6 @@ ip = wanip1.find('ExternalIPAddress')
 sub = wanip1.find('SubnetMask')
 dg = wanip1.find('DefaultGateway')
 
-#for X_BROADCOM_COM_CONNECTIONMODE in root.findall('X_BROADCOM_COM_CONNECTIONMODE'):
-#    ip = X_BROADCOM_COM_CONNECTIONMODE.find('ExternalIPAddress').text
-#    subnet = X_BROADCOM_COM_CONNECTIONMODE.get('SubnetMask')
-#    defaultgw = X_BROADCOM_COM_CONNECTIONMODE.get('DefaultGateway')
-#    print(ip, subnet, defaultgw)
-
-#for child in root.iter('WANIPConnection instance="1"'):
-#    print(SubnetMask.attrib)
 
 for child in root.findall('wanip1'):
     print(ip, sub, dg)
