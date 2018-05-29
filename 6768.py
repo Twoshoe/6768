6f7bd14ccfbe1fa780df7ff316ae2b0a6bfaeabe
twoshoe <twoshoe@gmail.com>
2018-05-29 18:22:51 -0400

    Still working on the element tree branches.

--- 6768.py
+++ 6768.py
@@ -5,14 +5,19 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('6768.conf')
 root = tree.getroot()
 
+wanip1 = 'WANIPConnection instance="1"'
+ip = ExternalIPAddress
+sub = SubnetMask
+dg = DefaultGateway
+
 #for X_BROADCOM_COM_CONNECTIONMODE in root.findall('X_BROADCOM_COM_CONNECTIONMODE'):
 #    ip = X_BROADCOM_COM_CONNECTIONMODE.find('ExternalIPAddress').text
 #    subnet = X_BROADCOM_COM_CONNECTIONMODE.get('SubnetMask')
 #    defaultgw = X_BROADCOM_COM_CONNECTIONMODE.get('DefaultGateway')
 #    print(ip, subnet, defaultgw)
 
+for child in root.iter('WANIPConnection instance="1"'):
+    print(SubnetMask.attrib)
+
+
 tree.write('out.xml')
-
-
-
-
