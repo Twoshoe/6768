f26abc6a9f45d9506d57cb11036f1eef28508944
twoshoe <twoshoe@gmail.com>
2018-05-29 20:11:53 -0400

    Found my IP just need to be able to manipulate.

--- 6768.py
+++ 6768.py
@@ -5,11 +5,13 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('6768.conf')
 root = tree.getroot()
 
-wanip1 = 'WANIPConnection instance"1"'
+wanip1 = 'WANIPConnection'
 ip = wanip1.find('ExternalIPAddress')
 sub = wanip1.find('SubnetMask')
 dg = wanip1.find('DefaultGateway')
 
+for child in root.iter('ExternalIPAddress'):
+    print(child.tag, child.text)
 
 for child in root.findall('wanip1'):
     print(ip, sub, dg)
