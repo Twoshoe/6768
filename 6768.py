5901cf390728ca47b269f34da11f0fdb37553d6a
twoshoe <twoshoe@gmail.com>
2018-05-30 15:14:49 -0400

    Cleaned up extra code.

--- 6768.py
+++ 6768.py
@@ -6,11 +6,6 @@ tree = ET.parse('6768.conf')
 root = tree.getroot()
 CommitChanges = False
 
-wanip1 = 'WANIPConnection'
-#ip = root.getchildren('InternetGatewayDevice/WANDevice[1]/WANConnectionDevice[2]:ExternalIPAddress');
-#sub = root.getchild('InternetGatewayDevice/WANDevice instance="1"/WANConnectionDevice instance="2":SubnetMask')
-#dg = root.getchild('InternetGatewayDevice/WANDevice instance="1"/WANConnectionDevice instance="2":DefaultGateway')
-
 for WANDevice in root.find('InternetGatewayDevice').findall('WANDevice'):
     if 'instance' not in WANDevice.attrib:
         continue
@@ -24,19 +19,14 @@ for WANDevice in root.find('InternetGatewayDevice').findall('WANDevice'):
                         continue
                     if (WANIPConnection.attrib['instance'] == '1'):
                         print(WANIPConnection.find('ExternalIPAddress').text, WANIPConnection.find('SubnetMask').text, WANIPConnection.find('DefaultGateway').text)
-                        WANIPConnection.find('ExternalIPAddress').text = '192.168.1.137'
+                        WANIPConnection.find('ExternalIPAddress').text = '10.60.1.10'
                         CommitChanges = True
                     
-    #print(child.findtext(''))
-
 #for child in root:
 #   for element in child:
 #        print (element.tag, ":", element.attrib)
 
 #DslCpeConfig > InternetGatewayDevice > WANDevice instance="1" > WANConnectionDevice instance="2" > WANIPConnection instance"1" > ExternalIPAddress, SubnetMask, DefaultGateway
-#currentip = ip.get("text");
 
-#for child in root:
-#    print(currentip)
 if (CommitChanges):
     tree.write('out.xml')
