dc5c4e5de954984a0ac752d61b5fa10d655f3761
Michael "Irish" O'Neill <irish.dot@gmail.com>
2018-05-30 20:19:15 -0400

    Write individual output files

--- 6768.py
+++ 6768.py
@@ -4,9 +4,12 @@
 
 import xml.etree.ElementTree as ET
 import argparse
+from os import mkdir, path
+
 
 TREE = ET.parse('6768.conf')
 ROOT = TREE.getroot()
+OUTPUT_DIR = 'configs'
 
 PARSER = argparse.ArgumentParser(description='Generate configuration files')
 PARSER.add_argument('IP_BASE', metavar='Base', type=str, nargs=1,
@@ -18,7 +21,10 @@ PARSER.add_argument('IP_END', metavar='End', type=int, nargs=1,
 
 ARGS = PARSER.parse_args()
 
-print(ARGS)
+try:
+    mkdir(OUTPUT_DIR)
+except FileExistsError:
+    pass
 
 # Moves down through the Element Tree to get to ExternalIPAddress, Subnet, Default Gateway
 for WANDevice in ROOT.find('InternetGatewayDevice').findall('WANDevice'):
@@ -41,8 +47,8 @@ for WANDevice in ROOT.find('InternetGatewayDevice').findall('WANDevice'):
 
                             # This Line sets the IP Address to whatever value is given.
                             WANIPConnection.find('ExternalIPAddress').text = new_ip
-                            print(new_ip)
-                            # TREE.write('out.xml')
+                            conf = path.join(OUTPUT_DIR, '6768-' + str(i) + '.conf')
+                            TREE.write(conf)
 
 # This was to print part of the tree
 # for child in root:
