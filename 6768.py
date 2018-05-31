081ec5b3a49d3a5065186f56cf21308baf13744d
Michael "Irish" O'Neill <irish.dot@gmail.com>
2018-05-30 20:08:53 -0400

    Use argument parser

--- 6768.py
+++ 6768.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
+'''Generate configuration files for an arbitrary range of IP addresses.'''
+
 import xml.etree.ElementTree as ET
+import argparse
 
 TREE = ET.parse('6768.conf')
 ROOT = TREE.getroot()
-IP_BASE = '10.60.1.'
-IP_START = 10
-IP_END = 246
+
+PARSER = argparse.ArgumentParser(description='Generate configuration files')
+PARSER.add_argument('IP_BASE', metavar='Base', type=str, nargs=1,
+        help='IP prefix (e.g. 10.60.1)')
+PARSER.add_argument('IP_START', metavar='Start', type=int, nargs=1,
+        help='IP prefix (e.g. 10)')
+PARSER.add_argument('IP_END', metavar='End', type=int, nargs=1,
+        help='IP prefix (e.g. 245)')
+
+ARGS = PARSER.parse_args()
+
+print(ARGS)
 
 # Moves down through the Element Tree to get to ExternalIPAddress, Subnet, Default Gateway
 for WANDevice in ROOT.find('InternetGatewayDevice').findall('WANDevice'):
@@ -24,8 +36,8 @@ for WANDevice in ROOT.find('InternetGatewayDevice').findall('WANDevice'):
                     if (WANIPConnection.attrib['instance'] == '1'):
                         print(WANIPConnection.find('ExternalIPAddress').text, WANIPConnection.find('SubnetMask').text, WANIPConnection.find('DefaultGateway').text)
                         # Iterates on the IP Address between 10 and 245 and saves each IP as a new file.
-                        for i in range(IP_START, IP_END):
-                            new_ip = IP_BASE + str(i)
+                        for i in range(ARGS.IP_START[0], ARGS.IP_END[0] + 1):
+                            new_ip = ARGS.IP_BASE[0] + '.' + str(i)
 
                             # This Line sets the IP Address to whatever value is given.
                             WANIPConnection.find('ExternalIPAddress').text = new_ip
